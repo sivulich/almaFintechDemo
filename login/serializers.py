05f8f0ff8de54297397408feec8f07d1a4a26bf5
sivulich <sivulich@itba.edu.ar>
2020-12-19 14:00:29 -0300

    Fixed user creation

--- login/serializers.py
+++ login/serializers.py
@@ -11,13 +11,18 @@ class ProfileSerializer(serializers.ModelSerializer):
 
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
-        fields = ['id', 'first_name', 'last_name', 'username', 'email', 'is_staff', 'is_active']
+        fields = ['id', 'first_name', 'last_name', 'username', 'password', 'email', 'is_staff', 'is_active']
         lookup_field = 'id'
 
     def create(self, validated_data):
         user = super(UserSerializer, self).create(validated_data)
+        user.set_password(validated_data['password'])
+        user.is_active = True
+        user.save()
         Profile.objects.create(user=user)
         return user
 
